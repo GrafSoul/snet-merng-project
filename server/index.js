625541c42d497fdd2e1cbdd93fca47d4e8dbc644
GrafSoul <grafsoul@gmail.com>
2021-02-01 13:48:27 +0500

    feat: Connected to MongoDB witn mogoose. And added .env file.

---
+++ server/index.js
@@ -0,0 +1,48 @@
+// Environment
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Core
+import path from 'path';
+
+// GraphQL Core
+import gql from 'graphql-tag';
+import { ApolloServer } from 'apollo-server';
+
+// MongoDB Core
+import mongoose from 'mongoose';
+
+// Parameters
+const CONNECT_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.nvckw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello world!',
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs, resolvers
+})
+
+mongoose.connect(CONNECT_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => {
+        console.log(`**************************************`);
+        console.log(`MongoDB Connected! Successful!`);
+        server.listen({port: PORT});
+    }).then(() => {
+        console.log(`**************************************`);
+        console.log(`Server is running on port: ${PORT}`);
+        console.log(`URL address: http://localhost:${PORT}`);
+        console.log(`**************************************`);
+    }).catch(error => {
+        console.log(error)
+    });
