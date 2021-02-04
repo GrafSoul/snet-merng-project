0ff11d4eb205b63bcf320cd39ac6ff57414d336c
GrafSoul <grafsoul@gmail.com>
2021-02-04 12:43:42 +0500

    feat: Added React app client to the project.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+// Styles
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Hi World! This is SoNet - Social Network</h1>
+        </div>
+    );
+}
+
+export default App;
