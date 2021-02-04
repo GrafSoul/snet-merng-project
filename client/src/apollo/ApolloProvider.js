6cf044674a1281c835de0a1955ebda34c5b3c617
GrafSoul <grafsoul@gmail.com>
2021-02-04 14:14:31 +0500

    feat: Added Apollo Client to the project app.

---
+++ client/src/apollo/ApolloProvider.js
@@ -0,0 +1,24 @@
+// Core
+import React from 'react';
+// Apollo
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+// Component
+import App from '../App';
+
+const httpLink = new createHttpLink({
+    uri: 'http://localhost:5000',
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+export default (
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+);
