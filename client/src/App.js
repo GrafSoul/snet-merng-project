bf082ed3422f3458205adb11c409fd5b2969e3de
GrafSoul <grafsoul@gmail.com>
2021-02-04 17:50:33 +0500

    feat: Added MenuBar, Home, Login, Register components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
+// Core
+import React from 'react';
+// Router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// Components
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 // Styles
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
     return (
-        <div>
-            <h1>Hi World! This is SoNet - Social Network</h1>
-        </div>
+        <Router>
+            <Container>
+                <MenuBar />
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Container>
+        </Router>
     );
 }
 
